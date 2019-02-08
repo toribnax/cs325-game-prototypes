88a345b41d2ae3d551297d0f63845a792be37d4c
Tori Broadnax <toribnax@gmail.com>
2019-02-08 12:41:13 -0500

    game one updated

--- game1/js/main.js
+++ game1/js/main.js
@@ -10,43 +10,149 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
-    var game = new Phaser.Game( 512, 256, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
-        game.load.image('h1', 'assets/hills1.png');
-    }
-    
-    var bouncy;
-    
-    function create() {
+    var game = new Phaser.Game(512, 256, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-         game.add.sprite(0, 0, 'h1');
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+function preload() {
+
+    //All the layers that compose the background
+    game.load.image('h1', 'assets/hills1.png');
+    game.load.image('h2', 'assets/hills2.png');
+    game.load.image('h3', 'assets/hills3.png');
+    game.load.image('h4', 'assets/hills4.png');
+    game.load.image('h5', 'assets/hills5.png');
+    game.load.image('h6', 'assets/hills6.png');
+    
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('cloud', 'assets/cloud.png');
+
+    game.load.spritesheet('fox', 'assets/fox.png', 24, 24);
+    game.load.spritesheet('veggies', 'assets/Food.png', 16, 16);
+
+
+}
+
+var player;
+var platforms;
+var cursors;
+
+var score = 0;
+var scoreText;
+
+
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+
+    //Loads in the background for the game
+    game.add.sprite(0, 0, 'h1');
+    game.add.sprite(0, 0, 'h2');
+    game.add.sprite(0, 0, 'h3');
+    game.add.sprite(0, 0, 'h4');
+    game.add.sprite(0, 0, 'h5');
+    game.add.sprite(0, 0, 'h6');
+
+    // This section of code was taken from the Making Your First Game Tutorial
+    platforms = game.add.group();
+    platforms.enableBody = true;
+    var ground = platforms.create(0, game.world.height - 2, 'ground');
+    ground.scale.setTo(2, 2);
+    ground.body.immovable = true;
+
+    
+    //This section of code was taken from example Group Vs Group
+    veggies = game.add.group();
+    veggies.enableBody = true;
+    veggies.physicsBodyType = Phaser.Physics.ARCADE;
+    for (var i = 0; i < 100; i++)
+    {
+        var c = veggies.create(game.world.randomX, Math.random() * 500, 'veggies', game.rnd.integerInRange(0, 53));
+        c.name = 'veg' + i;
+        c.body.immovable = true;
+    }
+
+
+    //Creates two clouds as for platforms
+    var ledge = platforms.create(100, 150, 'cloud');
+    ledge.body.immovable = true;
+    ledge.scale.setTo(0.2,0.2);
+
+    ledge = platforms.create(380, 80, 'cloud');
+    ledge.body.immovable = true;
+    ledge.scale.setTo(0.2,0.2);
+
+    //This section of code was taken from the Making Your First Game tutorial
+    player = game.add.sprite(24, game.world.height - 150, 'fox');
+    game.physics.arcade.enable(player);
+
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+    //Animation to move the fox
+    player.animations.add('move', [5, 0, 1, 2, 3, 4], 10, true);
+
+    
+    //  The score
+    scoreText = game.add.text(200, 12, 'score: 0', { font: '24px Kristen ITC', fill: '#82CAFA' });
+
+    cursors = game.input.keyboard.createCursorKeys();
+   
+
+    
+}
+
+function update() {
+
+    //  Collide the player the platforms
+    var hitPlatform = game.physics.arcade.collide(player, platforms);
+
+    game.physics.arcade.collide(veggies, platforms);
+
+    //  Checks to see if the player overlaps with any of the food, if it does call the collectFood function
+    game.physics.arcade.overlap(player, veggies, collectFood, null, this);
+
+    player.body.velocity.x = 0;
+
+    //Moves the fox to the left
+    if (cursors.left.isDown)
+    {
+
+        player.scale.x = -1;
+        player.body.velocity.x = -150;
+
+        player.animations.play('move');
+    }
+    //Moves the fox to the right
+    else if (cursors.right.isDown)
+    {
+
+        player.scale.x = 1;
+        player.body.velocity.x = 150;
+
+        player.animations.play('move');
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+
+        player.frame = 5;
     }
     
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down && hitPlatform)
+    {
+        
+        player.body.velocity.y = -250;
     }
+
+}
+
+function collectFood (player, veg) {
+    
+    veg.kill();
+    score += 5;
+    scoreText.text = 'score: ' + score;
+
+}
 };
