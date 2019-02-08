f24c304aebf21d7af9a3292316c2620f7a7a0246
Tori Broadnax <toribnax@gmail.com>
2019-02-08 12:47:44 -0500

    game one updated

--- game1/js/main.js
+++ game1/js/main.js
@@ -10,7 +10,7 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
-    var game = new Phaser.Game(512, 256, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    var game = new Phaser.Game(512, 256, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
 
